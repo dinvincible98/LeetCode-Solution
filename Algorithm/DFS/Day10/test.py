bcea5f2448c5693713a880dd498d9e79de401691
Mingqing <Mingqing>
2020-12-25 17:18:40 -0600

    update

--- Algorithm/DFS/Day10/test.py
+++ Algorithm/DFS/Day10/test.py
@@ -1,5 +1,18 @@
-def factorial(n):
-    while n>0:
-        res = n*factorial(n-1)
-    return res
-factorial(3)
+from collections import deque
+q = deque()
+begin = "hit"
+q.append((begin,1))
+word, dep = q.popleft()
+print(word)
+
+a = [1,1,2,2,3,3]
+b = set(a)
+print(b)
+
+for i in range(0,10):
+    if i<5:
+        continue
+    print(i)
+
+C = sum(a)
+print(C)
