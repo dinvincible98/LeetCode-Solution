6b3dbada8ac6e6dccdfdf006a034a89c77927677
Mingqing <Mingqing>
2020-12-30 17:32:53 -0600

    update

--- Algorithm/DFS/Day10/test.py
+++ Algorithm/DFS/Day10/test.py
@@ -16,3 +16,16 @@ for i in range(0,10):
 
 C = sum(a)
 print(C)
+
+for i in range(0,3):
+    print(i)
+
+print([1 for i in range(5)])
+
+c=[1,2,3]
+d=[4,5,6]
+e = zip(c,d)
+for i in e:
+    print(i)
+f = [1,1,2,3,1,4]
+print(set(f))
